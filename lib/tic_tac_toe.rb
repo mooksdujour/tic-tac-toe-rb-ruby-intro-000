8592262f5a06eb2591524ad203135148200c716d
John Moo-King <jmooking@hotmail.com>
2019-05-09 17:28:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,3 +56,18 @@ def turn(board)
     turn(board)
   end
 end
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |play|
+    if play == "X" || play == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn = turn_count(board).even? ? "X" : "O"
+end
