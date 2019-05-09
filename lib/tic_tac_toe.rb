9781657fa0ddce943e2de262c88814efcfbca103
John Moo-King <jmooking@hotmail.com>
2019-05-09 16:42:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,4 @@
+#Win combinations in tic tac toe
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -8,3 +9,15 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
+#Empty board
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
